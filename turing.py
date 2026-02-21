259ae8390abd27f8719302c1f478a5f39db2d19e
MerzlikinDev <mihailmerzlikin60@gmail.com>
2026-02-21 14:22:44 +0300

    Update turing.py

--- turing.py
+++ turing.py
@@ -1,4 +1,4 @@
-#v.0.0.4
+#v.0.0.5
             #Turing macchine by mishanya
 print('Now, please, write your rules for our alphabet from 3 letters: a, b and c. In format: b R q0. IN THREE LINES:')
 a = input()
@@ -20,30 +20,26 @@ def process(w):
             return w[:-1]
         if i > len(word) - 1:
             return w[:-1]
+
         if w[i] == 'a':
             w[i] = rules['a'][0]
 
             condition = rules['a'][2]
-            if rules['a'][1] == 'R':
-                i += 1
-            else:
-                i -= 1
+
         elif w[i] == 'b':
              w[i] = rules['b'][0]
 
              condition = rules['b'][2]
-             if rules['b'][1] == 'R':
-                 i += 1
-             else:
-                 i -= 1
+
         elif w[i] == 'c':
              w[i] = rules['c'][0]
 
              condition = rules['c'][2]
-             if rules['c'][1] == 'R':
-                 i += 1
-             else:
-                 i -= 1
+        if w[i] in 'abc':
+            if rules[w[i]][1] == 'R':
+                i += 1
+            else:
+                i -= 1
         else:
             break
     return f'Okay, Result is: {w[:-1]}'
