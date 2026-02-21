f721dc44868fcf9d0c6612da94946f7a2e7f6242
MerzlikinDev <mihailmerzlikin60@gmail.com>
2026-02-21 08:51:46 +0300

    first step with Turing machine
    basic algorithm with turing machine

---
+++ turing.py
@@ -0,0 +1,43 @@
+
+            #Turing macchine by mishanya
+rules = {'a': ('b', 'R', 'q0'), 'b': ('c', 'R', 'q0'), 'c': ('a', 'L', 'q1'), '-': ('!')}
+
+word = input()
+
+def process(w):
+    w = [i for i in w]
+    if not '-' in w:
+       return 'inf'
+    condition = 'q0'
+    i = 0
+    while True:
+        if i < 0:
+            return w[:-1]
+        if w[i] == 'a':
+            w[i] = rules['a'][0]
+
+            condition = rules['a'][2]
+            if rules['a'][1] == 'R':
+                i += 1
+            else:
+                i -= 1
+        elif w[i] == 'b':
+             w[i] = rules['b'][0]
+
+             condition = rules['b'][2]
+             if rules['b'][1] == 'R':
+                 i += 1
+             else:
+                 i -= 1
+        elif w[i] == 'c':
+             w[i] = rules['c'][0]
+
+             condition = rules['c'][2]
+             if rules['c'][1] == 'R':
+                 i += 1
+             else:
+                 i -= 1
+        else:
+            break
+    return w[:-1]
+print(process(word))
