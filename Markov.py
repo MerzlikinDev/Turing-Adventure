9706680660fb12a5275a2c3d8a10892f9f3efea9
MerzlikinDev <mihailmerzlikin60@gmail.com>
2026-03-08 15:01:49 +0300

    Update Markov.py

--- Markov.py
+++ Markov.py
@@ -4,12 +4,12 @@ def markovs_machine():
     rules = {}
     x = input().split()
     while x != ["end"]:
-        rules[x[0]] = x[1]
+        rules[x[0]] = [x[1], x[2]]
         x = input().split()
-    for keys in rules:
-        d = rules[keys]
-        if d in rules.keys():
-            if rules[d] == keys:
+    for key in rules:
+        d = rules[key][0]
+        if d in list(rules.keys()):
+            if (rules[d][0] == key) and (rules[d][1] == rules[key][1]):
                 return "цикл"
     sm_fd = True
     while sm_fd:
@@ -24,11 +24,16 @@ def markovs_machine():
                 break
         time1 = time.time()
         for i in list(rules.keys()):
-            while i in word:
-                word = word.replace(i, rules[i])
-                time_dis = time.time() - time1
-                if time_dis > 7.5: 
-                    print('цикл')
-                    return word
+            if rules[i][1] == "->":
+                while i in word:
+                    word = word.replace(i, rules[i][0])
+                    time_dis = time.time() - time1
+                    if time_dis > 7.5:
+                        print('цикл')
+                        return word
+            elif rules[i][1] == "=>":
+                while i in word:
+                    word = word.replace(i, rules[i][0])
+                return word
     return word
 print(markovs_machine())
