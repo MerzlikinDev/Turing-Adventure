af8d2815dc9c61a62dad58e3be65c50e4b4c3062
MerzlikinDev <mihailmerzlikin60@gmail.com>
2026-03-18 18:36:51 +0300

    Update new_markov.py

--- new_markov.py
+++ new_markov.py
@@ -1,49 +1,44 @@
 import time
+
+
 def markovs_machine() -> str:
     word = input()
-    rules = {}
+    rules = []
+
     x = input().split(" ")
     while x != ["."]:
-        rules[x[0]] = [x[1], x[2]]
-        print(rules)
+        if len(x) >= 3:
+            l = x[0]
+            a = x[1]
+            r = x[2]
+            rules.append((l, a, r))
         x = input().split(" ")
-    first_fd = True
-    while first_fd:
-        count = 0
 
-        if list(rules.keys())[0] not in word:
-            count += 1
-            if count == len(list(rules.keys())):
-                first_fd = False
-                return word
-        time1 = time.time()
-        i = 0
-        while i <= len(rules):
-            if list(rules.keys())[0] == i:
-                if rules[str(list(rules.keys())[i])][0] == "->":
-                    while list(rules.keys())[i] in word:
-                        word = word.replace(list(rules.keys())[i], rules[str(list(rules.keys())[i])][1])
+    start_time = time.time()
 
-                        time_dis = time.time() - time1
-                        if time_dis > 7.5:
-                            print('цикл')
-                            return word
-                elif rules[str(list(rules.keys())[i])][0] == "=>":
-                     while list(rules.keys())[i] in word:
-                        word = word.replace(list(rules.keys())[i], rules[str(list(rules.keys())[i])][1])
-                        return word
-                i += 1
-            else:
-                if rules[str(list(rules.keys())[i])][0] == "->":
-                        if list(rules.keys())[i] in word:
-                            word = word.replace(list(rules.keys())[i], rules[str(list(rules.keys())[i])][1], 1)
-                elif rules[str(list(rules.keys())[i])][0] == "=>":
-                        if list(rules.keys())[i] in word:
-                            word = word.replace(list(rules.keys())[i], rules[str(list(rules.keys())[i])][1], 1)
-                            return word
-                i += 1
-                if str(list(rules.keys())[0]) in word:
-                    i = 0
-                    continue
-    return word
-print(markovs_machine())
+    while True:
+        if time.time() - start_time > 7.5:
+            print('превышенно время выполнения')
+            return word
+
+
+        pr = False
+        for l, a, r in rules:
+            if l in word:
+                if a == "->":
+
+                    word = word.replace(l, r, 1)
+                    pr = True
+                    break
+                elif a == "=>":
+
+                    word = word.replace(l, r, 1)
+                    return word
+
+        if not pr:
+            return word
+
+
+
+result = markovs_machine()
+print(result)
